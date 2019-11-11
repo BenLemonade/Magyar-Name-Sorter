6e88ab4e9c33cff1de7f02828a0ead85798565a3
BenLemonade <adambenlemlih24@gmail.com>
2019-11-11 17:07:17 +0100

    TEST2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MagyarNameSorter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TESTING GIT");
         }
     }
 }
