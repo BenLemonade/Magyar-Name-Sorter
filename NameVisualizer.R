a310d5229cef344076c1e2bd6d2629871f8e3064
BenLemonade <adambenlemlih24@gmail.com>
2019-11-12 17:54:24 +0100

    Update to the data processing

--- NameVisualizer.R
+++ NameVisualizer.R
@@ -1,6 +1,7 @@
 ################################################################################################################################################
 ########################################## Processing and visualizing data from 'Magyar Names Sorter' ##########################################
 ################################################################################################################################################
+											By BenLemonade
 
 ############################################################# Setting Up Workspace #############################################################
 
@@ -17,7 +18,7 @@ library(tidyverse)
 
 ## Create needed variables ##
 SortedNames <- NULL
-
+LetterCategories <- NULL
 
 ################################################ Importing and cleaning info from 'Sorted.txt' #################################################
 
@@ -26,23 +27,36 @@ SortedNames <- "Variable is createed, process is ready"
 print(SortedNames)
 
 ## Read in 'Sorted.txt' and store into 'SortedNames' (This imports the data with the names in rows horizontally) ##
-SortedNames <- read.delim("Sorted.txt", header = FALSE, sep = ",")
+SortedNames <- read.delim("Sorted.txt", header = FALSE, sep = ",", )
 
 ## Transpose the chart so that the data is now displayed in columns, and store back into 'SortedNames' ##
 SortedNames <- t(SortedNames)
+summary(SortedNames)	#Gives summary of matrix
 
-LetterCategories <- c(SortedNames)
+## Gets all of the items in the first row as 'Letter Categories', then only selects every other, storing only the "X : #" ##
+LetterCategories <- c(SortedNames[1,])
+LetterCategories <- LetterCategories[c(TRUE,FALSE)]
+print(LetterCategories)
 
-SortedNames[1,1]
-SortedNames[1,3]
-SortedNames[1,5]
+
+dat2 <- read.csv("data2.csv", header=T, na.strings=c("","NA"))
 
 
-for(col in SortedNames)
-{
-	
-}
-summary(SortedNames)
+ColCounter <- c("V1","V3","V5","V6","V7","V8","V9","V11","V13","V15","V17","V19","V21","V23","V25","V27","V29","V31","V33","V35","V37","V39","V41","V43","V45","V47","V49","V51","V53","V55","V57","V59","V61")
+
+OrderedCols <- SortedNames
+OrderedCols <- OrderedCols[,!names(OrderedCols) %in% ColCounter]
+
+summary(OrderedCols)
+
+print(OrderedCols)
+dim(OrderedCols)
+class(OrderedCols)
+
+OrderedCols <- SortedNames
+
+
+
 
 
 
